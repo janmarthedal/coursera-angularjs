abfce5ab34b8963767b376c0e1b2a6d3b2da6862
Jan Marthedal Rasmussen <jan@janmr.com>
2017-10-30 10:22:48 +0100

    Module 3 solution done

---
+++ module3-solution/app.js
@@ -0,0 +1,62 @@
+(function() {
+    'use strict';
+
+    angular.module('NarrowItDownApp', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItemsDirective);
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+    function NarrowItDownController(MenuSearchService) {
+        var ctrl = this;
+        ctrl.term = '';
+        ctrl.found = [];
+        ctrl.emptySearch = false;
+        ctrl.doSearch = function() {
+            if (ctrl.term === '') {
+                ctrl.emptySearch = true;
+            } else {
+                ctrl.emptySearch = false;
+                MenuSearchService.getMatchedMenuItems(ctrl.term)
+                    .then(function(foundItems) {
+                        ctrl.found = foundItems;
+                        ctrl.emptySearch = !foundItems.length;
+                    });
+            }
+        };
+        ctrl.remove = function(index) {
+            ctrl.found.splice(index, 1);
+        };
+    }
+
+    MenuSearchService.$inject = ['$http'];
+    function MenuSearchService($http) {
+        var service = this;
+
+        service.getMatchedMenuItems = function(searchTerm) {
+            return $http({
+                url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+            }).then(function (result) {
+                var foundItems = result.data.menu_items
+                    .filter(function(item) {
+                        return item.description.indexOf(searchTerm) >= 0;
+                    });
+
+                return foundItems;
+            });
+        };
+    }
+
+    function FoundItemsDirective() {
+        var ddo = {
+            restrict: 'E',
+            templateUrl: 'foundItems.html',
+            scope: {
+                foundItems: '<',
+                onRemove: '&'
+            }
+        };
+        return ddo;
+    }
+
+})();

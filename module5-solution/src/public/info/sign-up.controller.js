47aa28d229c266f22ffb6c5726c0d95a7039b2eb
Jan Marthedal Rasmussen <jan@janmr.com>
2017-11-15 12:21:26 +0100

    Mod5 sol: Store and show user info

--- module5-solution/src/public/info/sign-up.controller.js
+++ module5-solution/src/public/info/sign-up.controller.js
@@ -4,19 +4,15 @@
 angular.module('public')
 .controller('SignUpController', SignUpController);
 
-SignUpController.$inject = ['MenuService'];
-function SignUpController(MenuService) {
+SignUpController.$inject = ['MenuService', 'UserInfoService'];
+function SignUpController(MenuService, UserInfoService) {
     var reg = this;
 
-    reg.user = {
-        firstname: '',
-        lastname: '',
-        email: '',
-        fav: ''
-    };
+    reg.user = UserInfoService.getUserInfo();
     reg.message = '';
 
     reg.submit = function() {
+        UserInfoService.setUserInfo(reg.user);
         reg.message = 'Your information has been saved';
     };
 }
